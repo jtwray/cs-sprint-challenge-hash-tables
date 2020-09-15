d36805521352274c9237158674f8718668e3ac9c
tucker <tuckerwray84@gmail.com>
2020-09-15 15:33:53 -0400

    ## ex2  - better plan for reconstruct_flight_plan  - first draft iteration coded out

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,34 +1,75 @@
+# first plan
+# 
+# Create Ticket Nodes from string pairs
+# load Ticket Nodes into a dict
+# on each load into the cache, check that each Node isnt a duplicate
+# if not a duplicate do both
+# 1 add into the dict
+# 2 also add to the end of an array at specific point of the dict
+# this will allow you to return all values in the order they were addded with no duplicates in constant time   
+
+
+# second plan
+# 
+# each Node has a S(start) and an F(finish)
+# the F of one Node is the S of one and only one other Node
+# we could add a pointer to each of the Nodes on entry
+# on hashcache Node creation create an assignment_pointer for the F of the Node finish - Node.f
+# on hashcache Node creation create an assignment_pointer for the S of the next Node - Node.Next
+# Node.Next = hashcahe[Node.f]
+# check for hashcache[finish] 
+#
+#
+# best plan so far
+
+#  we've created a list of the tickets in random order
+#  we've looped through the list and added them to the dict in the random order
+## we should while loop through the list finding the Node with 'None' as the start value
+#  this will be the first addition to the cache and the first addition to hashcache[total_transit] {total_transit:[]}
+#  continue while looping through the array Node.
+#
+# First Draft of Plan 3
+#
+#  next_flight=null
+#  total_transit=[] here or in the if loop?
+#  for flight in flights:
+#       if flight.source == None:
+#           hashcache[flight.source] = flight.destination
+#           hashcache['total_transit']=[]
+#           hashcache['total_transit'].append(flight.destination)
+#           nextflight = flight.destination
+#           return or yield?
+#  while flight.destination is not None:
+#       hashcache[flight.source] = flight.destination
+#       hashcache['total_transit'].append(flight.destination)
+#       nextflight = flight.destination
+#  if flight.destination is None:
+#       hashcache[flight.source]=flight.destination
+#       hashcache['total_transit'].append(flight.destination)            
+#       return hashcache['totat_transit]            
+#                       
+#  this will be the first addition to the cache and the first addition to hashcache[total_transit] {total_transit:[]}
+#  continue while looping through the array Node.
+
+
 class Ticket:
     def __init__(self, source, destination):
         self.source = source
         self.destination = destination
- 
+    def __str__(self):
+        return self.source,self.destination
 
 flight_cache={}
-
+def create_tickets(arr):
+    for r in arr:
+        ticket_values={'source':r.source,'destination':r.destination}
+        arr[r]=Ticket(**ticket_values)
+        print(arr)
 def LoadingTickets(tickets):
     for ticket in tickets:
         source=ticket[source]
         destination=ticket[destination]
         flight_cache[source]=destination
-
-
-def reconstruct_flight_plan(flights):
-    LoadingTickets(flights)
-    takeoff=0
-    flight_plan_order=[]
-    takeoff = flight_cache["None"]
-
-    for flight in flights:
-        flight_plan_order[flight]=flight_cache[takeoff]
-        takeoff=flight_cache[takeoff]
-        if takeoff is None:
-            flight_plan_order.append[takeoff]
-            return flight_plan_order
-
-def reconstruct_trip(tickets):
-    reconstruct_flight_plan(tickets)
-
 tickets = [
     Ticket("PIT", "ORD" ),
     Ticket("XNA", "CID" ),
@@ -41,132 +82,169 @@ tickets = [
     Ticket("SLC", "PIT" ),
     Ticket("BHM", "FLG" )
 ]
+def print_tickets(tickets):
+    for ticket in tickets:
+        print(ticket.__str__())
+print_tickets(tickets)
+create_tickets(tickets)
+# ticket_cache={}
+# for ticket in tickets:
+    # ticket_cache.update(ticket)
+# tickets_arr=[]
+# def reconstruct_flight_plan(flights):
+#     LoadingTickets(flights)
+#     takeoff=0
+#     flight_plan_order=[]
+#     takeoff = flight_cache["None"]
+
+#     for flight in flights:
+#         flight_plan_order[flight]=flight_cache[takeoff]
+#         takeoff=flight_cache[takeoff]
+#         if takeoff is None:
+#             flight_plan_order.append[takeoff]
+#             return flight_plan_order
+
+# def reconstruct_trip(tickets):
+#     reconstruct_flight_plan(tickets)
+
+# tickets = [
+#     Ticket("PIT", "ORD" ),
+#     Ticket("XNA", "CID" ),
+#     Ticket("SFO", "BHM" ),
+#     Ticket("FLG", "XNA" ),
+#     Ticket("NONE", "LAX"),
+#     Ticket("LAX", "SFO" ),
+#     Ticket("CID", "SLC" ),
+#     Ticket("ORD", "NONE"),
+#     Ticket("SLC", "PIT" ),
+#     Ticket("BHM", "FLG" )
+# ]
 
  
  
-reconstruct_trip(tickets)
-'''
-Understand
+# reconstruct_trip(tickets)
+# '''
+# Understand
 
-a list of tickets
-put back in order
+# a list of tickets
+# put back in order
 
-ticket has a source
-ticket has a destination
-ticket has an idx
+# ticket has a source
+# ticket has a destination
+# ticket has an idx
 
-tickets.idx=0.destination==tickets.idx==1.source
+# tickets.idx=0.destination==tickets.idx==1.source
 
-tickets[idx].destination==tickets[idx+1].source
+# tickets[idx].destination==tickets[idx+1].source
 
-tickets out of order
+# tickets out of order
 
-the are connected one by one
+# the are connected one by one
 
-each desitination is the next tickets source
+# each desitination is the next tickets source
 
-loop through the tickets
-
-
-'''
-
-'''
-
-10 cities
-
-5-6 tickets
+# loop through the tickets
 
 
-each ticket 2 cities
+# '''
 
-list of all cities
+# '''
 
-all cities have a source value
-all cites have a desitnation value
+# 10 cities
 
-there is a direct order of tickets
-
-ticket1 [destination]
-ticket2 [source]
+# 5-6 tickets
 
 
-whats the keys
+# each ticket 2 cities
 
-whats the values
+# list of all cities
 
-sources
+# all cities have a source value
+# all cites have a desitnation value
 
-destinations
+# there is a direct order of tickets
 
-idx
-
-
-idx=0
-
-0[source]=1[destination]
-
-i[source]=(i+1)[destination]
+# ticket1 [destination]
+# ticket2 [source]
 
 
-## Hints
+# whats the keys
 
-* The crux of this problem requires us to 'link' tickets together to
-  reconstruct the entire trip. For example, if we have a ticket `('SJC',
-  'BOS')` that has us flying from San Jose to Boston, then there exists
-  another ticket where Boston is the starting location, `('BOS',
-  'JFK')`. 
+# whats the values
 
-* We can hash each ticket such that the starting location is the key and
-  the destination is the value. Then, when constructing the entire
-  route, the `i`th location in the route can be found by checking the
-  hash table for the `i-1`th location.
+# sources
+
+# destinations
+
+# idx
 
 
+# idx=0
+
+# 0[source]=1[destination]
+
+# i[source]=(i+1)[destination]
 
 
-tickets = [
-    Ticket{ source: "PIT", destination: "ORD" },
-    Ticket{ source: "XNA", destination: "CID" },
-    Ticket{ source: "SFO", destination: "BHM" },
-    Ticket{ source: "FLG", destination: "XNA" },
-    Ticket{ source: "NONE", destination: "LAX" },
-    Ticket{ source: "LAX", destination: "SFO" },
-    Ticket{ source: "CID", destination: "SLC" },
-    Ticket{ source: "ORD", destination: "NONE" },
-    Ticket{ source: "SLC", destination: "PIT" },
-    Ticket{ source: "BHM", destination: "FLG" }
-]
-'''
+# ## Hints
+
+# * The crux of this problem requires us to 'link' tickets together to
+#   reconstruct the entire trip. For example, if we have a ticket `('SJC',
+#   'BOS')` that has us flying from San Jose to Boston, then there exists
+#   another ticket where Boston is the starting location, `('BOS',
+#   'JFK')`. 
+
+# * We can hash each ticket such that the starting location is the key and
+#   the destination is the value. Then, when constructing the entire
+#   route, the `i`th location in the route can be found by checking the
+#   hash table for the `i-1`th location.
 
 
-    #
-    # Plan
-    # 
-    # I now have all the sources in the table
-    # I can call the first source of none to start it
-    # set a runnervalue or pointer to keep crrnt airport
-    # currnt[source]
-    # 
 
-    # takeoff=0
-    #flight_plan_order=[]
-    #
-    # takeoff = flight_cache["None"]
-    # for flight in flights:
-    #   flight_plan_order[flight]=cache[takeoff]
-    #   takeoff=cache[takeoff]
-    #   if takeoff is None:
-    #       flight_plan_order.append[takeoff]   
-    #       return 
-    #       
-    #       
-    #       
-    # i have the sources all in the hash lookup
-    # i have an array to add them to one at a time as I find them
-    # i use the value of each loookup as the key for the next one
-    # then i lookup that value as a key to get the next and repeat  
-    #
-    #
-    # 
-    # 
-    # #
+
+# tickets = [
+#     Ticket{ source: "PIT", destination: "ORD" },
+#     Ticket{ source: "XNA", destination: "CID" },
+#     Ticket{ source: "SFO", destination: "BHM" },
+#     Ticket{ source: "FLG", destination: "XNA" },
+#     Ticket{ source: "NONE", destination: "LAX" },
+#     Ticket{ source: "LAX", destination: "SFO" },
+#     Ticket{ source: "CID", destination: "SLC" },
+#     Ticket{ source: "ORD", destination: "NONE" },
+#     Ticket{ source: "SLC", destination: "PIT" },
+#     Ticket{ source: "BHM", destination: "FLG" }
+# ]
+# '''
+
+
+#     #
+#     # Plan
+#     # 
+#     # I now have all the sources in the table
+#     # I can call the first source of none to start it
+#     # set a runnervalue or pointer to keep crrnt airport
+#     # currnt[source]
+#     # 
+
+#     # takeoff=0
+#     #flight_plan_order=[]
+#     #
+#     # takeoff = flight_cache["None"]
+#     # for flight in flights:
+#     #   flight_plan_order[flight]=cache[takeoff]
+#     #   takeoff=cache[takeoff]
+#     #   if takeoff is None:
+#     #       flight_plan_order.append[takeoff]   
+#     #       return 
+#     #       
+#     #       
+#     #       
+#     # i have the sources all in the hash lookup
+#     # i have an array to add them to one at a time as I find them
+#     # i use the value of each loookup as the key for the next one
+#     # then i lookup that value as a key to get the next and repeat  
+#     #
+#     #
+#     # 
+#     # 
+#     # #
